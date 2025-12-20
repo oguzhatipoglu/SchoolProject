da0bc1ee1fbe31b1e3da935d0ac230bdc25174d8
oguzhatipoglu <oguz520666@gmail.com>
2025-12-20 18:43:18 +0300

    Delete src/TreamentRequeast.java

--- src/TreamentRequeast.java
+++
@@ -1,9 +0,0 @@
-public class TreamentRequeast {
-    int patientID;
-    long arrivalTime;
-
-    public TreamentRequeast(int patientID , long arrivalTime){
-        this.arrivalTime = System.currentTimeMillis();
-        this.patientID = patientID;
-    }
-}
