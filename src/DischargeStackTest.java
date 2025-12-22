523403619f5c7c08e3c1d1f2b24ffc39dd0a93af
oguzhatipoglu <oguz520666@gmail.com>
2025-12-22 03:01:57 +0300

    task 3

--- src/DischargeStackTest.java
+++ src/DischargeStackTest.java
@@ -2,11 +2,11 @@ public class DischargeStackTest {
     public static void main(String[] args){
         DischargeStack s1 = new DischargeStack();
         //adding element
-        s1.push(new DischargeRecord(1));
-        s1.push(new DischargeRecord(2));
-        s1.push(new DischargeRecord(3));
-        s1.push(new DischargeRecord(4));
-        s1.push(new DischargeRecord(5));
+        s1.push(new DischargeRecord(100));
+        s1.push(new DischargeRecord(200));
+        s1.push(new DischargeRecord(300));
+        s1.push(new DischargeRecord(400));
+        s1.push(new DischargeRecord(500));
         System.out.println("All person added ");
         s1.printStack();
 
